720d53b894f6bba86cbc76e2bb4e38bd151d76ee
Sergio Ruiz <sergioruizsanchezz@gmail.com>
2024-09-04 10:58:43 -0500

    Found a way to make a dropdown list but is quite unelegant should work on a js solution for it

--- src/components/Patient.vue
+++ src/components/Patient.vue
@@ -3,8 +3,12 @@
 defineProps<{ patient }>()
 
 function phoneNumber(phone: string) {
-	console.log(phone)
 	return phone === 'None' ? 'Sin número' : phone
+}
+
+function returnList(field: string) {
+	const items = field.split(',');
+	return items
 }
 
 </script>
@@ -25,7 +29,15 @@ function phoneNumber(phone: string) {
 	<!--<td>{{ patient.status }}</td>-->
 	<td>{{ patient.care_plan }}</td>
 	<!--<td>{{ patient.intervention }}</td>-->
-	<td>{{ patient.clinical_data_uncontrolled }}</td>
+	<td class="clinical_data_uncontrolled">
+		Clínicos
+		<button>&#9660
+			<ul class="uncontrolled">
+				<li v-for="item in returnList(patient.clinical_data_uncontrolled)">{{ item }}</li>
+			</ul>
+		</button>
+		
+	</td>
 	<!--<td>{{ patient.name_tag }}</td>-->
 	<td>{{ patient.ips_name }}</td>
 	<!--<td>{{ patient.ips_id }}</td>
@@ -38,4 +50,48 @@ function phoneNumber(phone: string) {
 		border: 1px solid #ddd;
 		padding: 8px;
 	}
+
+	.clinical_data_uncontrolled {
+		max-height: 16px;
+		position: relative;
+		/* overflow-y: hidden;
+		overflow-x: hidden; */
+
+		button {
+			font-size: 12px;
+			padding: 0px;
+
+
+			.uncontrolled {
+				display: none;
+				list-style: none;
+				position: absolute;
+				background-color: bisque;
+				max-height: 80px;
+				overflow-y: scroll;
+				overflow-x: scroll;
+				top: 50%;
+				left: 0%;
+			}
+		}
+
+		/**IF I HAVE TIME I SHOULD FIND A MORE ELEGANT SOLUTION TO THIS */
+		button:focus {
+			.uncontrolled {
+				display: block;
+			}
+		}
+	}
+
+	/* .uncontrolled {
+		display: none;
+		list-style: none;
+		position: absolute;
+		background-color: bisque;
+		max-height: 80px;
+		overflow-y: scroll;
+		overflow-x: scroll;
+		top: 50%;
+		left: 0%;
+	} */
 </style>
